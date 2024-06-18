7acd4ac0d14e32a382a8411c49a7c6a8f74ba374
Sonali Burman <sonali.b.020200@gmail.com>
2024-06-18 21:55:13 +0530

    final commit

--- Task Management System/index.js
+++ Task Management System/index.js
@@ -9,4 +9,8 @@ app.use(express.json());
 app.use("/user", userRouter);
 app.use("/task", taskRouter);
 
+app.get('/', (req, res) => {
+    res.status(200).json({msg: 'Welcome to the Task Management App!'})
+})
+
 module.exports = { app, connection };
